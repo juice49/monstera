a0ef9f392f5362a08a85c41c383bc54464cc33e9
Ash <ashley@juice49.com>
2020-12-30 18:08:24 +0000

    Publicly export `cssValueToString` function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as space } from './mixins/space'
 export { default as stack, Props as StackProps } from './mixins/stack'
 export { default as generateCustomPropRange } from './lib/generate-custom-prop-range'
+export { default as cssValueToString } from './lib/css-value-to-string'
 export * from './types'
